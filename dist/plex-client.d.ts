88888b7189873ff37a205bf0c120a1d1ed0a2a0c
Alex Aritan <cokethenwpepsi@gmail.com>
2020-09-27 21:58:39 -0400

    Removing dist folder

--- dist/plex-client.d.ts
+++
@@ -1,9 +0,0 @@
-import { RequestOptions, BaseCapabilitiesResponse, LibrarySectionResponse, PlexClientOptions } from './types';
-export declare class PlexClient {
-    #private;
-    constructor(options: PlexClientOptions);
-    getBaseServerCapabilities(): Promise<BaseCapabilitiesResponse>;
-    getLibrarySections(): Promise<LibrarySectionResponse>;
-    getServerUrl(): string;
-    _request(options: RequestOptions): Promise<any>;
-}
--- dist/types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.METHOD = void 0;
-var METHOD;
-(function (METHOD) {
-    METHOD["GET"] = "GET";
-    METHOD["POST"] = "POST";
-    METHOD["PUT"] = "PUT";
-})(METHOD = exports.METHOD || (exports.METHOD = {}));
